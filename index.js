7d740d5ee34f18cacee3e8c90358db60b3f154e2
AH-dark <ahdark@outlook.com>
2022-04-09 23:32:36 +0800

    Feat: Encrypt data with Base64

--- index.js
+++ index.js
@@ -1,42 +1,47 @@
-const express = require("express");
-const uglifyJs = require("uglify-js");
-const bodyParser = require("body-parser");
-const csso = require("csso");
+const express = require( "express" );
+const uglifyJs = require( "uglify-js" );
+const bodyParser = require( "body-parser" );
+const csso = require( "csso" );
 
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.all("/", (req, res) => {
-    res.redirect(process.env.ORIGIN || "https://www.example.com");
+app.all( "/", ( req, res ) => {
+    res.redirect( process.env.ORIGIN || "https://www.example.com" );
     res.end();
-});
+} );
 
-app.use(bodyParser.json());
+app.use( bodyParser.json() );
 
-app.post("/compress/js", (req, res) => {
-    const { code, map } = uglifyJs.minify(req.body.code, {
+app.use( ( req, res, next ) => {
+    res.setHeader( "Access-Control-Allow-Origin", "*" );
+    next();
+} );
+
+app.post( "/compress/js", ( req, res ) => {
+    const { code, map } = uglifyJs.minify( req.body.code, {
         sourceMap: true
-    });
+    } );
     res.statusCode = 200;
-    res.json({
-        code: code,
-        map: map,
-    });
+    res.json( {
+        code: Buffer.from( code ).toString( "base64" ),
+        map: Buffer.from( map ).toString( "base64" )
+    } );
     res.end();
-});
+} );
 
-app.post("/compress/css", (req, res) => {
-    const { css, map } = csso.minify(req.body.code, {
-        sourceMap: true,
-    });
+app.post( "/compress/css", ( req, res ) => {
+    const { css, map } = csso.minify( req.body.code, {
+        sourceMap: true
+    } );
     res.statusCode = 200;
-    res.json({
-        code: css,
-        map: JSON.stringify(map),
-    });
+    res.json( {
+        code: Buffer.from( css ).toString( "base64" ),
+        map: Buffer.from( JSON.stringify( map ) ).toString( "base64" )
+    } );
     res.end();
-});
+} );
 
-app.listen(port, () => {
-    console.log("[Express]", `APP Listening on port ${port}.`);
-});
+app.listen( port, () => {
+    console.log( "[Express]", `APP Listening on port ${port}.` );
+} );
