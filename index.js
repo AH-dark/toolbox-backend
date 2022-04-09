0452c5ba4dc316fe699931c8647723c0527466c2
AH-dark <ahdark@outlook.com>
2022-04-10 00:22:14 +0800

    Feat: Base64 two-way encryption

--- index.js
+++ index.js
@@ -1,47 +1,55 @@
-const express = require( "express" );
-const uglifyJs = require( "uglify-js" );
-const bodyParser = require( "body-parser" );
-const csso = require( "csso" );
+const express = require("express");
+const uglifyJs = require("uglify-js");
+const bodyParser = require("body-parser");
+const csso = require("csso");
 
 const app = express();
 const port = process.env.PORT || 9000;
 
-app.all( "/", ( req, res ) => {
-    res.redirect( process.env.ORIGIN || "https://www.example.com" );
-    res.end();
-} );
-
-app.use( bodyParser.json() );
-
-app.use( ( req, res, next ) => {
-    res.setHeader( "Access-Control-Allow-Origin", "*" );
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "*");
+    res.setHeader("Access-Control-Max-Age", "3600");
     next();
-} );
+});
 
-app.post( "/compress/js", ( req, res ) => {
-    const { code, map } = uglifyJs.minify( req.body.code, {
-        sourceMap: true
-    } );
-    res.statusCode = 200;
-    res.json( {
-        code: Buffer.from( code ).toString( "base64" ),
-        map: Buffer.from( map ).toString( "base64" )
-    } );
+app.all("/", (req, res) => {
+    res.redirect(process.env.ORIGIN || "https://www.example.com");
     res.end();
-} );
+});
 
-app.post( "/compress/css", ( req, res ) => {
-    const { css, map } = csso.minify( req.body.code, {
-        sourceMap: true
-    } );
+app.use(bodyParser.json());
+
+app.post("/compress/js", (req, res) => {
+    const { code, map } = uglifyJs.minify(
+        Buffer.from(req.body.code, "base64").toString(),
+        {
+            sourceMap: true,
+        }
+    );
     res.statusCode = 200;
-    res.json( {
-        code: Buffer.from( css ).toString( "base64" ),
-        map: Buffer.from( JSON.stringify( map ) ).toString( "base64" )
-    } );
+    res.json({
+        code: Buffer.from(code).toString("base64"),
+        map: Buffer.from(map).toString("base64"),
+    });
     res.end();
-} );
+});
 
-app.listen( port, () => {
-    console.log( "[Express]", `APP Listening on port ${port}.` );
-} );
+app.post("/compress/css", (req, res) => {
+    const { css, map } = csso.minify(
+        Buffer.from(req.body.code, "base64").toString(),
+        {
+            sourceMap: true,
+        }
+    );
+    res.statusCode = 200;
+    res.json({
+        code: Buffer.from(css).toString("base64"),
+        map: Buffer.from(JSON.stringify(map)).toString("base64"),
+    });
+    res.end();
+});
+
+app.listen(port, () => {
+    console.log("[Express]", `APP Listening on port ${port}.`);
+});
