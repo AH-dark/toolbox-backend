a321c419c269fbda807e49c3b3425d5be737d931
AH-dark <ahdark@outlook.com>
2022-04-10 14:28:09 +0800

    Feat: Handle errors, output errors and warnings

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const uglifyJs = require("uglify-js");
-const bodyParser = require("body-parser");
 const csso = require("csso");
 
 const app = express();
@@ -18,19 +17,32 @@ app.all("/", (req, res) => {
     res.end();
 });
 
-app.use(bodyParser.json());
+app.use(express.json({ limit: "2100000kb" }));
 
 app.post("/compress/js", (req, res) => {
-    const { code, map } = uglifyJs.minify(
-        Buffer.from(req.body.code, "base64").toString(),
+    const { code, map, error, warnings } = uglifyJs.minify(
+        Buffer.from(req.body.code, "base64").toString("utf8"),
         {
             sourceMap: true,
         }
     );
+    if (error) {
+        res.statusCode = 200;
+        res.json({
+            error: Buffer.from(error.message, "utf8").toString("base64"),
+        });
+        res.end();
+        return;
+    }
     res.statusCode = 200;
     res.json({
-        code: Buffer.from(code).toString("base64"),
-        map: Buffer.from(map).toString("base64"),
+        code: Buffer.from(code, "utf8").toString("base64"),
+        map: req.query.map
+            ? Buffer.from(map, "utf8").toString("base64")
+            : undefined,
+        warnings: warnings
+            ? Buffer.from(JSON.stringify(warnings), "utf8").toString("base64")
+            : undefined,
     });
     res.end();
 });
